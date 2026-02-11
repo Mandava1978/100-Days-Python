27fa9327b00f4b6e1d9f829a7b045982d0b0bd39
Mandava Srinivasarao <srinumandava@hotmail.com>
2026-02-11 19:29:39 +0530

    Day8

---
+++ Day8/Functions_with_inputs.py
@@ -0,0 +1,30 @@
+# Caesar Cipher
+'''
+def my_function():
+    # Do this
+    # Then Do this
+    # Finally do this
+
+def my_function(something):
+    # Do this with something
+    # Then do this
+    # Finally do this 
+
+    
+'''
+
+def great():
+    print("Hello")
+    print("how do you do?")
+    print("Isn't the weather nice?")
+
+great()
+
+# Functions that allows for inputs
+
+def great_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+    print(f"Good bye {name}")
+
+great_with_name("Mandava Srinivasarao")
---
+++ Day8/function_with_MultipleParams.py
@@ -0,0 +1,7 @@
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+greet_with("Mandava Srinivasarao", "USA")
+greet_with("USA","Mandava Srinivasarao")
+greet_with(name="Mandava Srinivasarao", location="USA")
